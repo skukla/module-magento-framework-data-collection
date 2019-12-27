1bc0f2463c7bf6c39eb9a69267d9261044fdf137
Steve Kukla <stevenjkukla@gmail.com>
2019-12-27 00:03:45 -0500

    Fixed expected plugin class error

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Skukla_MagentoFrameworkDataCollection',
+    'Skukla_Framework',
     __DIR__
 );
